0313e2de204e5d9d7b4cc3543a9beac860bfbded
Samuel Brian <sbrian93@gmail.com>
2020-11-03 21:20:51 +0800

    Added validation using Joi

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,11 +1,30 @@
+const Joi = require('joi');
 const User = require('../models/User');
+
+const updateProfileSchema = Joi.object({
+    name: Joi.string()
+            .alphanum()
+            .min(3)
+            .max(100)
+            .required()
+});
 
 module.exports = {
     updateProfile : async (req, res) => {
-        try{
+        
+        let validation = updateProfileSchema.validate(req.body);
+        if(validation.error)    
+        {
+            res.status(400).send({
+                element: validation.error.details[0].path,
+                error: validation.error.details[0].message
+            });
+        }
+        
+        try
+        {
             let email = req.auth.email;
             let user = await User.findOne({email});
-            console.log(user);
 
             user.name = req.body.name;
             if(user.isnew)
